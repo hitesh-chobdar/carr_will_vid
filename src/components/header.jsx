ea9c578f536459a9a2d36b78414ed04c97d77fff
hitesh-chobdar <102777186+hitesh-chobdar@users.noreply.github.com>
2024-04-30 14:17:10 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@
             <div class="header">
   <a href="" class="logo">www.life-chill.you</a>
   <div class="header-right">
-    <a class="" href="https://sharedby.blomp.com/B2noR5">Chemistry_pedagogy</a>
+    <a class="" href="https://sharedby.blomp.com/B2noR5">Chemistry</a>
     <a href="https://sharedby.blomp.com/YwJbh2">English_pedagogy</a>
     <a href="https://sharedby.blomp.com/zff5Lk">Maths_pedagogy</a>
     <a href="https://sharedby.blomp.com/B2noR5">Science_pedagogy</a>
