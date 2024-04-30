c2fbfe557c89e4f3b3f54e96e34901eb4a0d86e8
hitesh-chobda <hityoug.chobdar@gmail.com>
2024-04-30 13:14:17 +0530

    helo

---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+ export default function Header(){
+
+    return(
+        <div>
+            <div class="header">
+  <a href="" class="logo">www.life-chill.you</a>
+  <div class="header-right">
+    <a class="" href="https://sharedby.blomp.com/B2noR5">Chemistry_pedagogy</a>
+    <a href="https://sharedby.blomp.com/YwJbh2">English_pedagogy</a>
+    <a href="https://sharedby.blomp.com/zff5Lk">Maths_pedagogy</a>
+    <a href="https://sharedby.blomp.com/B2noR5">Science_pedagogy</a>
+    <a href="https://sharedby.blomp.com/C1aDX8">Maths_paper_2_part-A</a>
+    <a href="https://sharedby.blomp.com/opaGHy">Maths_paper_2_part-B</a>
+  </div>
+  <div className="ha">  H&A</div>
+</div>
+            
+       
+
+        </div>
+    )
+ }
